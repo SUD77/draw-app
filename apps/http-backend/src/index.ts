b7cad55b18a737d75b3cc1218788f794255cb2d2
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-09-07 03:48:57 +0530

    feat : added signUp endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,37 @@
-import express from 'express'
+import express from "express";
+import { Jwt } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
+const app = express();
+const jwt = require("jsonwebtoken");
 
-const app=express();
+app.post("/singup", (req, res) => {
+  //db call
+  res.json({
+    userId: "123",
+  });
+});
+
+app.post("/singin", (req, res) => {
+  const userId = 1;
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    token,
+  });
+});
+
+app.post("/room", middleware, (req, res) => {
+  res.json({
+    roomId: 123,
+  });
+});
 
 app.listen(3001);
